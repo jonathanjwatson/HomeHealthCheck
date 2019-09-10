2b8336f51e3c58ca095c028ee952d2be2bb425cc
jwats287 <jwats287@users.noreply.github.com>
2019-09-10 19:48:11 -0400

    Fixed my linter errors

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
-
-
-
-
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
