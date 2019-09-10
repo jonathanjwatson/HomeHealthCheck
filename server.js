ed15ac10ed0b2da7df47bf41f816e3287af7135c
jwats287 <jwats287@users.noreply.github.com>
2019-09-10 19:35:51 -0400

    ran npm fix

--- server.js
+++ server.js
@@ -7,30 +7,24 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-
-
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = {
+  force: false
+};
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
+// If running a test, set syncOptions.force to true clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
